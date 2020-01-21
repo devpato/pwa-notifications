445d298a328182562771a8d93260e377cfe94926
Pato <pevargas2730@gmail.com>
2020-01-21 15:58:06 -0500

    fixed sw.js

--- final/scripts/main.js
+++ final/scripts/main.js
@@ -1,65 +1,65 @@
-'use strict';
+"use strict";
 
-const notificationButton = document.getElementById('enableNotifications');
+const notificationButton = document.getElementById("enableNotifications");
 let swRegistration = null;
 
 initializeApp();
 
 function initializeApp() {
-  if ('serviceWorker' in navigator && 'PushManager' in window) {
-    console.log('Service Worker and Push is supported');
+  if ("serviceWorker" in navigator && "PushManager" in window) {
+    console.log("Service Worker and Push is supported");
 
     //Register the service worker
     navigator.serviceWorker
-      .register('./sw.js')
+      .register("../../sw.js")
       .then(swReg => {
-        console.log('Service Worker is registered', swReg);
+        console.log("Service Worker is registered", swReg);
 
         swRegistration = swReg;
         initializeUi();
       })
       .catch(error => {
-        console.error('Service Worker Error', error);
+        console.error("Service Worker Error", error);
       });
   } else {
-    console.warn('Push messaging is not supported');
-    notificationButton.textContent = 'Push Not Supported';
+    console.warn("Push messaging is not supported");
+    notificationButton.textContent = "Push Not Supported";
   }
 }
 
 function initializeUi() {
-  notificationButton.addEventListener('click', () => {
+  notificationButton.addEventListener("click", () => {
     displayNotification();
   });
 }
 
 function displayNotification() {
-  if (window.Notification && Notification.permission === 'granted') {
+  if (window.Notification && Notification.permission === "granted") {
     notification();
   }
   // If the user hasn't told if he wants to be notified or not
   // Note: because of Chrome, we are not sure the permission property
   // is set, therefore it's unsafe to check for the "default" value.
-  else if (window.Notification && Notification.permission !== 'denied') {
+  else if (window.Notification && Notification.permission !== "denied") {
     Notification.requestPermission(status => {
-      if (status === 'granted') {
+      if (status === "granted") {
         notification();
       } else {
-        alert('You denied or dismissed permissions to notifications.');
+        alert("You denied or dismissed permissions to notifications.");
       }
     });
   } else {
     // If the user refuses to get notified
     alert(
-      'You denied permissions to notifications. Please go to your browser or phone setting to allow notifications.'
+      "You denied permissions to notifications. Please go to your browser or phone setting to allow notifications."
     );
   }
 }
 
 function notification() {
   const options = {
-    body: 'Testing Our Notification',
-    icon: './bell.png'
+    body: "Testing Our Notification",
+    icon: "./bell.png"
   };
-  swRegistration.showNotification('PWA Notification!', options);
+  swRegistration.showNotification("PWA Notification!", options);
 }
